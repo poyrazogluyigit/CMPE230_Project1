ddaa6ac34a2ffa70afcd70dfa03cfa40fb24136c
Ultiminati <ultiminatea@gmail.com>
2023-03-25 17:43:17 +0300

    base of hashmap

--- main.c
+++ main.c
@@ -287,31 +287,6 @@ void* peek(struct Stack *stack){
     }
 }
 
-// standard function to convert long long to string:
-char* lltoa(long long value, char* result) {
-    int base = 10;
-
-    char* ptr = result, *ptr1 = result, tmp_char;
-    long long tmp_value;
-
-    do {
-        tmp_value = value;
-        value /= base;
-        *ptr++ = "zyxwvutsrqponmlkjihgfedcba9876543210123456789abcdefghijklmnopqrstuvwxyz"[35 + (tmp_value - value * base)];
-    } while ( value );
-
-    // Apply negative sign
-    if (tmp_value < 0) *ptr++ = '-';
-    *ptr-- = '\0';
-    while(ptr1 < ptr) {
-        tmp_char = *ptr;
-        *ptr--= *ptr1;
-        *ptr1++ = tmp_char;
-    }
-    return result;
-}
-
-
 void goTo(int token){
     push(tokenStack, TOKEN, token);
 }
